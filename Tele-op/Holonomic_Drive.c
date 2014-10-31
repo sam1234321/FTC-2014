410e85afc4710c1f31f8113aa57bcfcaa4cc81b3
James P <andrew.henry10a@gmail.com>
2014-10-31 15:20:59 -0700

    we invert

--- Tele-op/Holonomic_Drive.c
+++ Tele-op/Holonomic_Drive.c
@@ -35,10 +35,10 @@ void movement(int x1, int y1, int x2, int y2){
 	// we are only using x2 and y2 for the actual movement, x1 and y1 are for rotation. Optional, but reccomended and usef	ul.
 	if(abs(y2) > threshold || abs(x2) > threshold || abs(x1) > threshold && joy1Btn(8) == 0){ // make sure joystick is not in dead zone. I know, it only checks y2, but it is long a tedious to check for more, I'll do it later. ------------------------------------TODO-----------------------------------------------------------
 		speed = FULLSPEED;
-		motor[topLeft] = ((y2 * speed) / 127) - ((x1 * speed) / 127) + ((x2 * speed) / 127);		// ATTENTION!!!! THIS IS NOT WORKING CODE! THIS IS JUST FOR EASIER CODING WHEN THE PROTOTYPE IS DONE! SAME FOR CODE BELOW!
-		motor[topRight] = ((y2 * speed) / 127) + ((x1 * speed) / 127) - ((x2 * speed) / 127);
-		motor[bottomLeft] = ((y2 * speed) / 127) - ((x1 * speed) / 127) - ((x2 * speed) / 127);
-		motor[bottomRight] = ((y2 * speed) / 127) + ((x1 * speed) / 127) + ((x2 * speed) / 127);
+		motor[topLeft] = ((y2 * speed) / 127) + ((x1 * speed) / 127) + ((x2 * speed) / 127);		// ATTENTION!!!! THIS IS NOT WORKING CODE! THIS IS JUST FOR EASIER CODING WHEN THE PROTOTYPE IS DONE! SAME FOR CODE BELOW!
+		motor[topRight] = ((y2 * speed) / 127) - ((x1 * speed) / 127) - ((x2 * speed) / 127);
+		motor[bottomLeft] = ((y2 * speed) / 127) + ((x1 * speed) / 127) - ((x2 * speed) / 127);
+		motor[bottomRight] = ((y2 * speed) / 127) - ((x1 * speed) / 127) + ((x2 * speed) / 127);
 	}
 	else if(abs(y2) > threshold || abs(x2) > threshold || abs(x1) > threshold && joy1Btn(08) == 1){ // Is the same as the above if statement, but also detects precsion mode
 		speed = PRECISIONMODESPEED;
